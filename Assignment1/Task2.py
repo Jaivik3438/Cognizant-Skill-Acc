9dc306ce8103102be8a81782be289deff2ffada7
Jaivik Tailor <jaiviktailor@Jaiviks-MacBook-Pro.local>
2025-02-01 11:40:04 -0500

    Added Assignment1 Task2

---
+++ Assignment1/Task2.py
@@ -0,0 +1,29 @@
+# Defining two numbers to work with
+num1 = 30
+num2 = 20
+
+# Performing addition and printing the result
+print(f"The addition of {num1} and {num2} is", num1 + num2)
+print("--------------------------------------------------------------")
+
+# Performing subtraction and printing the result
+print(f"The subtraction of {num1} and {num2} is", num1 - num2)
+print("--------------------------------------------------------------")
+
+# Performing multiplication and printing the result
+print(f"The multiplication of {num1} and {num2} is", num1 * num2)
+print("--------------------------------------------------------------")
+
+# Performing division and printing the result
+print(f"The division of {num1} and {num2} is", num1 / num2)
+print("--------------------------------------------------------------")
+
+# Performing floor division and printing the result
+print(f"The floor division of {num1} and {num2} is", num1 // num2)
+print("--------------------------------------------------------------")
+
+# Performing modulo and printing the result (remainder after division)
+print(f"The modulo of {num1} and {num2} is", num1 % num2)
+print("--------------------------------------------------------------")
+
+
