4886c6dfc2e020b48251d36bcb5840f0b71137d0
Jaivik Tailor <jaiviktailor@Jaiviks-MacBook-Pro.local>
2025-02-01 13:56:29 -0500

    Added Assingment-2 Task-1 Counting Down with while loop

---
+++ Assignment2/Task1.py
@@ -0,0 +1,7 @@
+# Task 1 - Counting Down with Loops
+startingNumber = int(input("Enter count down staring number!!"))
+
+while startingNumber > 0:
+    print(startingNumber, end=" ")
+    startingNumber -= 1
+print("Blast off! 🚀")
