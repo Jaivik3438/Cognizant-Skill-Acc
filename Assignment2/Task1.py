62e560318faac8366a2c32bca3fed1dad43848a0
Jaivik Tailor <jaiviktailor@Jaiviks-MacBook-Pro.local>
2025-02-01 14:06:20 -0500

    Added conition to check integer number in Assingment-2 Task-1

--- Assignment2/Task1.py
+++ Assignment2/Task1.py
@@ -1,7 +1,10 @@
 # Task 1 - Counting Down with Loops
 startingNumber = int(input("Enter count down staring number!!"))
 
-while startingNumber > 0:
-    print(startingNumber, end=" ")
-    startingNumber -= 1
-print("Blast off! 🚀")
+if startingNumber > 0:
+    while startingNumber > 0:
+        print(startingNumber, end=" ")
+        startingNumber -= 1
+    print("Blast off! 🚀")
+else:
+    print("Please enter integer formate of number!")
