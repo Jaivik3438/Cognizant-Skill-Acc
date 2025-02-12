d4ae0e37e023700d56551fa3d2fc39ffbc756ba1
Jaivik Tailor <jaiviktailor@Jaiviks-MacBook-Pro.local>
2025-02-12 17:35:07 -0500

    Added Assignment 3 Task 2 String Methods

---
+++ Assignment3/Task2.py
@@ -0,0 +1,20 @@
+# Task 2 - String Methods
+
+# Create a string with the value " hello, python world! ".
+# Use the following string methods and print the results:
+# strip() to remove extra spaces
+# capitalize() to capitalize the first letter
+# replace() to replace "world" with "universe"
+# upper() to convert the string to uppercase
+
+string_name = " hello, python world! "
+
+removed_extra_space = string_name.strip()
+
+print("Remmoved Extra Space: " + removed_extra_space)
+
+print("Capitalized First Letter: " + removed_extra_space.capitalize())
+
+print("Replaced 'world' with 'universe' " + removed_extra_space.replace("world","universe"))
+
+print("Uppercase String: " + removed_extra_space.upper())
